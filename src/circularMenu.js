ae2dd82fce96768fa4cc5d5b441234572abecc81
Jan Brus <jan.brus@centrum.cz>
2024-08-28 09:11:18 +0200

    circular menu finished implementation

--- src/circularMenu.js
+++ src/circularMenu.js
@@ -1,15 +1,28 @@
-function handleClick() {
-  if (document.getElementsByClassName('petal1circle')[0]) {
-    document.getElementsByClassName('petal1circle')[0].classList = 'petal1top';
-    document.getElementsByClassName('petal2circle')[0].classList = 'petal2top';
-    document.getElementsByClassName('petal3circle')[0].classList = 'petal3top';
-    document.getElementsByClassName('petal4circle')[0].classList = 'petal4top';
-    document.getElementsByClassName('petal5circle')[0].classList = 'petal5top';
-  } else {
-    document.getElementsByClassName('petal1top')[0].classList = 'petal1circle';
-    document.getElementsByClassName('petal2top')[0].classList = 'petal2circle';
-    document.getElementsByClassName('petal3top')[0].classList = 'petal3circle';
-    document.getElementsByClassName('petal4top')[0].classList = 'petal4circle';
-    document.getElementsByClassName('petal5top')[0].classList = 'petal5circle';
-  }
+function setPetalTop() {
+  document.getElementById("flowerWrapper").classList = "flowerTop";
+  document.getElementById("petal1").classList = "petal1top";
+  document.getElementById("petal2").classList = "petal2top";
+  document.getElementById("petal3").classList = "petal3top";
+  document.getElementById("petal4").classList = "petal4top";
+  document.getElementById("petal5").classList = "petal5top";  
 }
+function setPetalCircle() {
+  document.getElementById("flowerWrapper").classList = "flowerCircle";
+  document.getElementById("petal1").classList = "petal1circle";
+  document.getElementById("petal2").classList = "petal2circle";
+  document.getElementById("petal3").classList = "petal3circle";
+  document.getElementById("petal4").classList = "petal4circle";
+  document.getElementById("petal5").classList = "petal5circle"; 
+}
+
+window.onscroll = (event) => {
+  let scroll = this.scrollY;
+  console.log(scroll);
+  let flowerElement = document.getElementById("flowerPositioner");
+
+  if (scroll > flowerElement.offsetTop) {
+    setPetalTop();
+  } else {
+    setPetalCircle();
+  }
+};
